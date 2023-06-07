4830aa4439afa33cd96a5225560ea655a6a22c7c
COR Coding <corcoding@android-3755cdb9ae9c6cf1.localdomain>
2023-06-07 11:14:59 -0400

    added shopping files context and search  to get search bar

---
+++ src/components/ShoppingListSearch.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+const ShoppingListSearch = () => {
+    return (
+        <div>
+            <header>
+                <p>Search Items to add to your inventory shopping list</p>
+            </header>
+
+            <input type="search" />
+
+        </div>
+    )
+
+}
+
+export default ShoppingListSearch
