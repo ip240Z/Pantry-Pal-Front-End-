115c72f43f7bbe19616b2c5116b3c62ca874070d
Ian <ip240z@protonmail.com>
2023-06-07 13:08:53 -0400

    Created a basic shopping list item component. The Shopping List now dynamically creates cards based on the shoppingList state prop

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import './App.css';
 import Header from './components/Header';
 import MainRenderArea from './components/MainRenderArea';
 import BottomNavBar from './components/BottomNavBar';
-import { BrowserRouter } from 'react-router-dom';
-import ShoppingListContext from './components/ShoppingListContext';
 
 function App() {
   return (
-    <ShoppingListContext.Provider>
-      <div className="App">
+    <div className="App">
         <Header />
         <MainRenderArea />
         <BottomNavBar />
-        <ShoppingListContext />
-      </div>
-    </ShoppingListContext.Provider>
+    </div>  
   );
 }
 
--- src/components/MainRenderArea.js
+++ src/components/MainRenderArea.js
@@ -1,21 +1,37 @@
 
 import Inventory from "./Inventory";
-
 import { Routes, Route } from "react-router-dom";
 import ShoppingListRender from "./ShoppingListRender";
-
+import { useState } from "react";
+import ShoppingListContext from "./ShoppingListContext"
 
 
 let MainRenderArea = () => {
+
+    const [shoppingList, setShoppingList] = useState([{ "id": 1, "name": "Bananas", "image": "banana.jpg" }, { "id": 2, "name": "Steak", "image": "steak.jpg" }])
+
+    const fetchShoppingList = async () => {
+        try { 
+            const response = await fetch(`${"Our API endpoint to fetch shopping list data"}`)
+            if(!response.ok) {
+                throw new Error("Error fetching shopping list data")
+            }
+            const data = await response.json();
+            console.log("Shopping list fetched", data);
+            setShoppingList(data)
+        } catch (error) {
+            console.log('An error occurred: ', error)
+        }
+    }
+
     return (
         <main>
-
-            <div><Inventory /></div>
-
-            <Routes>
-                <Route path="/shoppinglist" element={<ShoppingListRender/>}/>
-            </Routes>
-
+            <ShoppingListContext.Provider value={ { shoppingList, setShoppingList} }>
+                {/* <div><Inventory /></div> */}
+                <Routes>
+                    <Route path="/shoppinglist" element={<ShoppingListRender/>}/>
+                </Routes>
+            </ShoppingListContext.Provider>
         </main>
     )
 }
