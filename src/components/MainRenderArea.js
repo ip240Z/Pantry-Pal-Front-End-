ee3e65a22de6885829673c730b4ae8808d97b14f
kblack-git <keithblack@gmail.com>
2023-06-08 09:48:09 -0400

    inventory components

--- src/components/MainRenderArea.js
+++ src/components/MainRenderArea.js
@@ -4,12 +4,14 @@ import { Routes, Route } from "react-router-dom";
 import ShoppingListRender from "./ShoppingListRender";
 import { useState } from "react";
 import ShoppingListContext from "./ShoppingListContext"
+import InventoryRender from "./InventoryRender";
+import InventoryContext from "./InventoryContext";
 
 
 let MainRenderArea = () => {
 
     const [shoppingList, setShoppingList] = useState([{ "id": 1, "name": "Bananas", "image": "banana.jpg" }, { "id": 2, "name": "Steak", "image": "steak.jpg" }])
-
+    const [inventory, setInventory] = useState([{"id": 1, "name": "Apple", "quantity": 2, "age": "current date - date added", "image": "Apple.jpg"}])
     const fetchShoppingList = async () => {
         try { 
             const response = await fetch(`${"Our API endpoint to fetch shopping list data"}`)
@@ -27,13 +29,17 @@ let MainRenderArea = () => {
     return (
         <main>
             <ShoppingListContext.Provider value={ { shoppingList, setShoppingList} }>
-                {/* <div><Inventory /></div> */}
+                <InventoryContext.Provider value={ { inventory, setInventory} }>
                 <Routes>
+                    <Route path="/inventory" element={<InventoryRender/>}/>
                     <Route path="/shoppinglist" element={<ShoppingListRender/>}/>
                 </Routes>
+                </InventoryContext.Provider>
             </ShoppingListContext.Provider>
+
         </main>
     )
 }
 
 export default MainRenderArea;
+            
