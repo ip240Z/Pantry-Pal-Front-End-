089a2317d92e4a171602885cf31f4880767ed878
COR Coding <corcoding@android-3755cdb9ae9c6cf1.localdomain>
2023-06-12 15:20:31 -0400

    added login features and getting auth information together

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import Header from './components/Header';
 import MainRenderArea from './components/MainRenderArea';
 import BottomNavBar from './components/BottomNavBar';
 
+import { Routes, Route, } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
+import Preferences from './components/Preferences/Preferences';
+
+
+
 function App() {
   return (
-    <section className="App">
+    <>
+      <section className="App">
         <Header />
         <MainRenderArea />
         <BottomNavBar />
-    </section>
+      </section>
+      <div className='wrapper'>
+        <h1>Application</h1>
+
+{/* 
+          <Routes>
+            <Route path="/dashboard"> < Dashboard /></Route>
+            <Route path="/preferences"> < Preferences /></Route>
+          </Routes>
+   */}
+      </div>
+    </>
   );
 }
-
 export default App;
--- src/components/LoginPageContent.js
+++ src/components/LoginPageContent.js
@@ -1,44 +1,50 @@
 import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const LoginPageContent = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleLogin = () => {
-    navigate("/loginPage"); 
-  };
+    const handleLogin = () => {
+        navigate("/loginPage");
+    };
 
-  return (
-    <article>
-      <header>
-        <h4>Login</h4>
-      </header>
+    return (
+        <article>
+            <header>
+                <h4>Login</h4>
+            </header>
+            <div className="LoginWrapper">
+                <section className="inputCredentials" >
+                    <div className="inputEmail">
+                        <h6>PLEASE ENTER YOUR EMAIL</h6>
+                        <input type="email" name="email" id="" />
+                    </div>
 
-      <section className="inputCredentials" >
-        <div className="inputEmail">
-          <h6>PLEASE ENTER YOUR EMAIL</h6>
-          <input type="email" name="email" id="" />
-        </div>
+                    <div className="inputPassword">
+                        <h6>PLEASE ENTER YOUR PASSWORD</h6>
+                        <input type="password" name="password" id="" />
+                    </div>
+                </section>
 
-        <div className="inputPassword">
-          <h6>PLEASE ENTER YOUR PASSWORD</h6>
-          <input type="password" name="password" id="" />
-        </div>
-      </section>
+                <section>
+                    <div>
+                        <button type="submit" onClick={handleLogin}>
+                            LOG IN
+                        </button>
+                    </div>
+                    <div>
+                        <Link className="SignUpLink" to="/register">
+                            Sign Up
+                        </Link>
+                    </div>
+                </section>
+            </div>
+        </article>
 
-      <section>
-        <div>
-          <button type="submit" onClick={handleLogin}>
-            LOGIN
-          </button>
-        </div>
-        <div>
-          <Link className="SignupLink" to="/SignInPage">
-            SignUp
-          </Link>
-        </div>
-      </section>
-    </article>
-  );
+
+
+    );
 };
 
 export default LoginPageContent;
