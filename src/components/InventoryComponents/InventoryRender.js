73bedb2dd89aece4bc46c10b62b7db3c41670b0d
Ian <ip240z@protonmail.com>
2023-06-12 13:17:16 -0400

    Added some stylings to the search page results.

--- src/components/InventoryComponents/InventoryRender.js
+++ src/components/InventoryComponents/InventoryRender.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import InventorySearch from "./InventorySearch";
 import InventoryContainer from "./Inventory";
-import InventoryItemNearExpiration from "./InventoryItemNearExpiration";
 
 let InventoryRender = () => {
 
