111e2adb63943a9bc5f2ade360b4cb95751dfce4
Ian <ip240z@protonmail.com>
2023-06-14 10:00:28 -0400

    Created functional barcode scan endpoint

--- src/components/SearchPageComponents/SearchBarCodeScan.js
+++ src/components/SearchPageComponents/SearchBarCodeScan.js
@@ -1,24 +1,54 @@
 import Html5QrcodePlugin from "./Html5QrcodeScannerPlugin";
+import SearchItem from "./SearchItem";
+import { useState, useEffect } from "react";
 
 const SearchBarCodeScan = (props) => {
 
-    const onNewScanResult = (decodedText, decodedResult) => {
-        console.log(decodedText, decodedResult)
-    };
+    let APIKEY = process.env.REACT_APP_APIKEY
 
+    const [scannedItem, setScannedItem] = useState(null);
+  
+    const onNewScanResult = async (decodedText, decodedResult) => {
+      console.log(decodedText, decodedResult);
+      try {
+        const response = await fetch(
+          `https://api.spoonacular.com/food/products/upc/${decodedText}`,
+          {
+            headers: {
+              "x-api-key": APIKEY,
+            },
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Error finding scanned item");
+        }
+        let data = await response.json();
+        setScannedItem(data);
+        console.log(data);
+      } catch (error) {
+        console.error("An error occurred: ", error);
+      }
+    };
+  
+  
     return (
         <div>
-            <Html5QrcodePlugin 
-                fps={10}
-                qrbox={250}
-                disableFlip={false}
-                qrCodeSuccessCallback={onNewScanResult}
+            <section>
+                <Html5QrcodePlugin
+                    fps={10}
+                    qrbox={250}
+                    disableFlip={false}
+                    qrCodeSuccessCallback={onNewScanResult}
                 />
-            </div>
-        );
-    };
-    
-    export default SearchBarCodeScan
-            
-           
+            </section>
+            <section>
+                {scannedItem ? <SearchItem data={scannedItem} /> : "Waiting for scan"}
+            </section>
+        </div>
+    );
+};
+
+export default SearchBarCodeScan
+
+
 
